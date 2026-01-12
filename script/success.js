961a4fdba32612af37df5f62de5402f48bdad3cd
Weronika Sleboda <wsleboda@outlook.com>
2026-01-12 18:16:49 +0100

    Some cleaning

--- script/success.js
+++
@@ -1,8 +0,0 @@
-
-window.addEventListener('load', () => {
-  const okBtn = document.getElementById('ok-btn');
-  okBtn.addEventListener('click', () => {
-    window.location.href = './booking.html';
-  });
-
-});
